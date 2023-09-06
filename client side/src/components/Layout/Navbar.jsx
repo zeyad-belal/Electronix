d5052568d40fef5d37e5c98c5f7234e31baa4f26
zeyad belal <zeyadbelal00@gmail.com>
2023-09-06 19:07:06 +0300

    style edits

--- client side/src/components/Layout/Navbar.jsx
+++ client side/src/components/Layout/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = (props) => {
         <Link to={"/"}>
           <div className="flex items-baseline mr-5 w-44 sm:w-56">
             <img
-              className=" sm:mt-1"
+              className=""
               src="/assets/logo/main-orange-and-white.png"
               alt="Electronix"
             />
